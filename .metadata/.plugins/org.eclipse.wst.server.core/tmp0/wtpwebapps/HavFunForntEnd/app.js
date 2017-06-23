6bf34379b593f365d4851ca7db210e9011edd5e2
pgowthaman <gowthaman712@gmail.com>
2017-06-23 19:39:35 +0530

    frist commit

---
+++ .metadata/.plugins/org.eclipse.wst.server.core/tmp0/wtpwebapps/HavFunForntEnd/app.js
@@ -0,0 +1,51 @@
+
+
+var app = angular.module('myApp', [ 'ngRoute', 'ngCookies' ]);
+
+console.log('----Starting app.js')
+app.config(function($routeProvider) {
+	$routeProvider.when('/', {
+		templateUrl : 'c_user/login.html',
+	}).when('/register', {
+		templateUrl : 'c_user/register.html'
+	}).when('/login', {
+		templateUrl : 'c_user/login.html'
+	}).when('/listOfUsers', {
+		templateUrl : 'c_user/listOfUsers.html',
+		controller : 'userController'
+	}).when('/listofBlog', {
+		templateUrl : 'c_blog/listofBlog.html',
+		controller : 'blogController'
+	}).when('/addNew', {
+		templateUrl : 'c_blog/addNew.html',
+		controller : 'blogController'
+	}).when('/editBlog/:blogId', {
+		templateUrl : 'c_blog/editBlog.html',
+		controller : 'editBlogController'
+	}).when('/listOfJobs', {
+		templateUrl : 'c_job/jobList.html',
+		controller : 'jobController'
+	}).when('/addJob', {
+		templateUrl : 'c_job/addJob.html',
+		controller : 'jobController'
+	}).when('/editJob/:jobId', {
+		templateUrl : 'c_job/editJob.html',
+		controller : 'editJobController'
+	}).when('/friendList', {
+		templateUrl : 'c_friend/friendList.html',
+		controller : 'friendController'
+	}).when('/userList', {
+		templateUrl : 'c_friend/userList.html',
+		controller : 'friendController'
+	}).when('/chatPage', {
+		templateUrl : 'c_chat/chatPage.html',
+		controller : 'chatController'
+
+	}).when('/uploadPicture', {
+		templateUrl : 'c_user/uploadPicture.html'
+	})
+
+	.otherwise({
+		redirectTo : '/'
+	})
+});
---
+++ HavFunBackEnd/src/main/java/com/cart/config/WebAppConfig.java
@@ -0,0 +1,58 @@
+package com.cart.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.multipart.commons.CommonsMultipartResolver;
+import org.springframework.web.servlet.config.annotation.DefaultServletHandlerConfigurer;
+import org.springframework.web.servlet.config.annotation.EnableWebMvc;
+import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
+import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
+import org.springframework.web.servlet.mvc.WebContentInterceptor;
+import org.springframework.web.servlet.view.InternalResourceViewResolver;
+
+@Configuration
+@EnableWebMvc
+@ComponentScan("com.cart.controller")
+public class WebAppConfig extends WebMvcConfigurerAdapter {
+	@Bean
+	public InternalResourceViewResolver viewResolver() {
+		InternalResourceViewResolver viewResolver = new InternalResourceViewResolver();
+		viewResolver.setPrefix("/WEB-INF/views/");
+		viewResolver.setSuffix(".jsp");
+		return viewResolver;
+	}
+
+	public void addResourceHandlers(ResourceHandlerRegistry registry) {
+		registry.addResourceHandler("/resources/**").addResourceLocations("/WEB-INF/resources/");
+	}
+
+	public void configurerDefaultServletHandling(DefaultServletHandlerConfigurer configurer) {
+		configurer.enable();
+	}
+
+	@Bean
+	public WebContentInterceptor webContentInterceptor() {
+		WebContentInterceptor interceptor = new WebContentInterceptor();
+		interceptor.setCacheSeconds(0);
+		interceptor.setUseExpiresHeader(true);
+		interceptor.setUseCacheControlHeader(true);
+		interceptor.setUseCacheControlNoStore(true);
+		return interceptor;
+	}
+
+	@Override
+	public void addInterceptors(InterceptorRegistry registry) {
+		registry.addInterceptor(webContentInterceptor());
+	}
+
+	@Bean(name = "multipartResolver")
+	public CommonsMultipartResolver getCommonMultipartResolver() {
+		CommonsMultipartResolver multipartResolver = new CommonsMultipartResolver();
+		multipartResolver.setMaxUploadSize(20971520); // 20MB
+		multipartResolver.setMaxInMemorySize(1048576); // 1MB
+		return multipartResolver;
+	}
+
+}
